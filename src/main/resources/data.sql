1779af1b08517260436a0468c3a9fccb2ca16a4c
Gamut73 <lilwaltmus@gmail.com>
2024-01-16 16:03:18 +0200

    clean up testing data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,6 @@ INSERT INTO account(balance, account_type, customer_id) VALUES(1000000, 0, 1);
 INSERT INTO customer(email, first_name, last_name) VALUES('queries@bankz.com', 'BankZ', 'BankZ');
 INSERT INTO account(balance, account_type, customer_id) VALUES(1000000, 0, 2);
 
-INSERT INTO customer(email, first_name, last_name) VALUES('frodefavian@gmail.com', 'Konstantine', 'Levin');
+INSERT INTO customer(email, first_name, last_name) VALUES('levin@gmail.com', 'Konstantine', 'Levin');
 INSERT INTO account(balance, account_type, customer_id) VALUES(15000, 0, 3);
 INSERT INTO account(balance, account_type, customer_id) VALUES(500, 1, 3);
